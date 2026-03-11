9cf74dccef7e33dff03716f98d28c6c15826369c
Danil Parshakov <enqtk@protonmail.com>
2026-03-11 17:47:13 +0700

    Update migration

--- CRMService.Infrastructure/DataBase/Migrations/20260311084544_Update11032026.Designer.cs
+++ CRMService.Infrastructure/DataBase/Migrations/20260311084544_Update11032026.Designer.cs
@@ -4,6 +4,7 @@ using CRMService.Infrastructure.DataBase;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CRMService.Infrastructure.DataBase.Migrations
 {
     [DbContext(typeof(MainContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20260311084544_Update11032026")]
+    partial class Update11032026
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CRMService.Infrastructure/DataBase/Migrations/20260311084544_Update11032026.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRMService.Infrastructure.DataBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update11032026 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FieldType",
+                table: "KindsParameters",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "FieldType",
+                table: "KindsParameters",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
